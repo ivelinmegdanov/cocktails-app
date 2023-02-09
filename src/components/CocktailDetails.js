32627602bd51398eec41ebb13c7219fb75abef4e
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2023-02-09 18:07:23 +0200

    Fixed CocktailDetails

--- src/components/CocktailDetails.js
+++ src/components/CocktailDetails.js
@@ -1,46 +1,33 @@
-import React, { useState, useEffect } from "react";
-import {Route, Link, Routes, useParams} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 export const CocktailDetails = () => {
-    const cocktailId = useParams();
-    const [cocktail, setCocktail] = useState(null);
+  const { id } = useParams();
+  const [cocktail, setCocktail] = useState(null);
 
+  useEffect(() => {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then(response => response.json())
+      .then(data => setCocktail(data.drinks[0]))
+      .catch(error => console.error(error));
+  }, [id]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktailId}`);
-          const result = await response.json();
-          setCocktail(result.drinks);
-        };
-    
-        fetchData();
-      }, [cocktailId]);
-    
-      if (!cocktail) {
-        return <div>Loading...</div>;
-      }
-    
-      return (
-        <div>
-          <h2>{cocktail.strDrink}</h2>
-          <p>Category: {cocktail.strCategory}</p>
-          <p>Glass: {cocktail.strGlass}</p>
-          <p>Instructions: {cocktail.strInstructions}</p>
-          <h3>Ingredients:</h3>
-          <ul>
-            {Object.keys(cocktail)
-              .filter(key => key.startsWith("strIngredient"))
-              .map((key, index) => {
-                if (!cocktail[key]) {
-                  return null;
-                }
-                return (
-                  <li key={index}>
-                    {cocktail[key]} ({cocktail[`strMeasure${index + 1}`]})
-                  </li>
-                );
-              })}
-          </ul>
-        </div>
-      );
-    };
+  if (!cocktail) {
+    return <div>Loading...</div>;
+  }
+
+  console.log(cocktail)
+  return (
+    <div className="cocktail__details">
+      <div className="details__img">
+        <img className="card__img" src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+      </div>
+      <div className="details__props">
+        <h1>{cocktail.strDrink}</h1>
+        <p>Category: {cocktail.strCategory}</p>
+        <p>Glass: {cocktail.strGlass}</p>
+        <p>Instructions: {cocktail.strInstructions}</p>
+      </div>
+    </div>
+  );
+};
