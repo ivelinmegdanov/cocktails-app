7662bc8803b5a3dc7d9bcf321aa0cfb208c92157
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2023-02-12 15:48:10 +0200

    Added option to add a cocktail to favorites

--- src/components/CocktailCard.js
+++ src/components/CocktailCard.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 
 export const CocktailCard = (cocktail) => {
+  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem("favorites")) || []);
+
+  const handleAddToFavorites = () => {
+    let favoritesList = JSON.parse(localStorage.getItem("favorites")) || [];
+    const isFavorite = favoritesList.some((item) => item.idDrink === cocktail.cocktail.idDrink);
+  
+    if (isFavorite) {
+      favoritesList = favoritesList.filter((item) => item.idDrink !== cocktail.cocktail.idDrink);
+    } else {
+      const currentDate = new Date();
+      const cocktailWithDate = { ...cocktail.cocktail, dateFavorited: currentDate };
+      favoritesList.push(cocktailWithDate);
+    }
+  
+    localStorage.setItem("favorites", JSON.stringify(favoritesList));
+    setFavorites(favoritesList);
+  };
+
   return (
     <div className="cocktail__card">
       <div className="card__img">
@@ -12,6 +30,9 @@ export const CocktailCard = (cocktail) => {
         <p className="card__desc">{cocktail.cocktail.strInstructions}</p>
         <div className="card__buttons">
           <Link to={`/cocktails/${cocktail.cocktail.idDrink}`} className="card__button">Details</Link>
+          <button className="card__button" onClick={handleAddToFavorites}>
+            {favorites && favorites.some((item) => item.idDrink === cocktail.cocktail.idDrink) ? "★" : "☆"}
+          </button>
         </div>
       </div>
     </div>
